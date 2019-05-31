e75771ea1fd0cbb46f6bc6b38fa1ca6aa7fa5ac9
J0hn <45014144+j0hntv@users.noreply.github.com>
2019-05-31 17:58:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import logging
+import os
+import requests
+import telegram
+import time
+
+TIMEOUT = 5
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', filename='bot.log')
+
+token_devman = os.getenv('token_devman')
+token_bot = os.getenv('token_bot')
+chat_id = os.getenv('chat_id')
+
+def start_bot(bot, chat_id, token_devman):
+    timestamp = time.time()
+
+    long_polling_url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': f'Token {token_devman}'}
+
+    while True:
+        try:
+            payload = {'timestamp': timestamp}
+            response = requests.get(long_polling_url, headers=headers, params=payload)
+            response.raise_for_status()
+            response = response.json()
+
+            status = response.get('status', None)
+
+            if status == 'found':
+                new_attempts = response['new_attempts'][0]
+                lesson = new_attempts['lesson_title']
+                is_negative = new_attempts['is_negative']
+                timestamp = response['last_attempt_timestamp']
+
+                message_list = [f'Преподаватель проверил работу *"{lesson}".*']
+
+                if is_negative:
+                    message_list.append('К сожалению, в работе есть ошибки. 🙈')
+                else:
+                    message_list.append('Работа принята 🚀')
+
+                message = '\n'.join(message_list)
+                bot.send_message(chat_id=chat_id, text=message, parse_mode='Markdown')
+            
+            elif status == 'timeout':
+                timestamp = response['timestamp_to_request']
+
+            else:
+                logging.warning(response)
+
+        except requests.exceptions.ReadTimeout:
+            pass
+
+        except requests.ConnectionError as error:
+            logging.error(repr(error))
+            time.sleep(TIMEOUT)
+
+        except requests.exceptions.HTTPError as error:
+            logging.error(repr(error))
+            time.sleep(TIMEOUT)
+
+
+def main():
+    bot = telegram.Bot(token=token_bot)
+    start_bot(bot, chat_id, token_devman)
+
+
+if __name__ == '__main__':
+    main()
